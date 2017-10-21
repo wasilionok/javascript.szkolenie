f4649bcaeb61d109d25c530b7f0ddd20a7486643
Agnieszka Wasilionok <agnieszka@wasilionok.pl>
2017-10-21 15:14:16 +0200

    java3

---
+++ zadanie_1/java/java.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function mnozenie(parametr1, parametr2, parametr3){
+    var wynik = parametr1 * parametr2 * parametr3;
+    /*console.log('Wynik:' + wynik);*/
+    return wynik;
+}
+
+
+
+var wynikMnozenia = mnozenie(3,5,8);
+console.log('wynik mnożenia: ' + wynikMnozenia);
+
