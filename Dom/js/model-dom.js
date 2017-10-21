d25ac45dcce45456a5bed3846fb8a15ce087d018
Agnieszka Wasilionok <agnieszka@wasilionok.pl>
2017-10-21 16:07:18 +0200

    dom

---
+++ Dom/js/model-dom.js
@@ -0,0 +1,7 @@
+'use strict';
+console.log(window);
+
+console.log(document);
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
---
+++ Dom/js/znajdowanie-elemtow.js
@@ -0,0 +1,42 @@
+'use strict';
+var parFirst = document.getElementById('parFirst');
+console.log(parFirst);
+
+var link= document.getElementsByClassName('link');
+console.log(link);
+
+var linkPoTagu = document.getElementsByTagName('p')
+console.log(linkPoTagu);
+
+var wszytskieLinki =document.querySelectorAll('.link');
+console.log(wszytskieLinki);
+
+var pierwszyLinki =document.querySelector('.link');
+console.log(pierwszyLinki);
+
+
+var elementParent = document.getElementById( "parFirst" ).parentNode;
+console.log(elementParent);
+
+
+
+console.log(document.getElementById("parFirst").children);
+
+console.log( document.getElementById("parFirst").childNodes);
+
+console.log(document.getElementById("parFirst").childNodes[1]);
+console.log(document.getElementById("parFirst").firstChild);
+console.log(document.getElementById("parFirst").lastChild);
+
+
+
+
+
+
+
+
+
+
+
+
+
